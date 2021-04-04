5eaef4dc2fee67e6ec4e54cc9bcac9f3ed744e5a
Sophie Gu <sophieqgu@gmail.com>
2021-04-04 14:52:56 -0400

    commit tree 29b21cb59da0dacfdad74220aca7ae0bbb8e7351

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value 
+    })
   }
 
   render() {
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+          <strong>Your message:</strong>
+          <input type="text" name="message" id="message" 
+           value={this.state.message}
+           onChange={this.handleChange}/>
+           {this.props.maxChars - this.state.message.length} chars left
       </div>
     );
   }
